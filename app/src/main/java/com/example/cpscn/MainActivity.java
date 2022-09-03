64a82e5cd0fdc70dc65580bd64c22e3f615364e6
EMON <emonsarkar1111@gmail.com>
2022-09-03 15:46:39 +0600

    hi there mvvm

---
+++ app/src/main/java/com/example/cpscn/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.cpscn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView =(WebView) findViewById(R.id.webViewET);
+        WebSettings webSettings =webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://cpscn.edu.bd/");
+    }
+}
